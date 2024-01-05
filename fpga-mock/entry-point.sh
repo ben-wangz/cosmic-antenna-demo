ad44da4a4df008ea7cba75e88d45f2d25ebd0cfd
byang628@zhejianglab.com <AYAYAY>
2024-01-05 11:36:39 +0800

    init template

--- fpga-mock/entry-point.sh
+++ fpga-mock/entry-point.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-JAVA_OPS=${JAVA_OPS:--server -Xmx4096m -Xms1024m}
-java $JAVA_OPS -jar /app/application.jar
+#!/bin/bashJAVA_OPS=${JAVA_OPS:--server -Xmx4096m -Xms1024m}java $JAVA_OPS -jar /app/application.jar
